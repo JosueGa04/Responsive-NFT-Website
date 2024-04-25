696ba506a8e293c0636c0429a3ddeb2e35b0e32d
JosueGa04 <a01637983@tec.mx>
2024-04-24 20:01:28 -0600

    commit 24 abr

--- main.js
+++ main.js
@@ -1,47 +1,96 @@
-// Código existente
-document.querySelectorAll('.donut').forEach(function(donut) {
-    donut.addEventListener('click', function(event) {
-        event.stopPropagation(); // Evita que el evento de clic se propague al documento
+var data_V1 = [{
+    "Program": "Text",
+    "Percentage": 65,
+    "Description": "JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive."
+}, {
+    "Program": "Image",
+    "Percentage": 55,
+    "Description": "CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work. It can control the layout of multiple web pages all at once."
+}, {
+    "Program": "Data Simulation",
+    "Percentage": 48,
+    "Description": "Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis."
+}, {
+    "Program": "Code Generator Review",
+    "Percentage": 47,
+    "Description": "SQL (Structured Query Language) is a standardized programming language that's used to manage relational databases and perform various operations on the data in them."
+}, {
+    "Program": "Music & Sound",
+    "Percentage": 33,
+    "Description": "Bla bla bla"
+}];
 
-        // Si el segmento ya está seleccionado, deseleccionarlo
-        if (donut.classList.contains('selected')) {
-            donut.classList.remove('selected');
-            donut.classList.add('unselected');
-        } else { // Si el segmento no está seleccionado, seleccionarlo
-            donut.classList.add('selected');
-            donut.classList.remove('unselected');
-        }
+var width = parseInt(d3.select('#pieChart').style('width'), 10);
+var height = width;
+var radius = (Math.min(width, height) - 15) / 2;
 
-        // Agregar la clase 'unselected' a todos los segmentos no seleccionados
-        document.querySelectorAll('.donut:not(.selected)').forEach(function(otherDonut) {
-            otherDonut.classList.add('unselected');
-        });
+var type = function getObject(obj) {
+    types = [];
+    for (var i = 0; i < obj.length; i++) {
+        types.push(obj[i].Program);
+    }
+    return types
+};
+var arcOver = d3.svg.arc()
+    .outerRadius(radius + 10)
+    .innerRadius(150);
 
-        // Si todos los segmentos están deseleccionados, eliminar la clase 'unselected' de todos los segmentos
-        if (document.querySelectorAll('.donut.selected').length === 0) {
-            document.querySelectorAll('.donut').forEach(function(donut) {
-                donut.classList.remove('unselected');
-            });
-        }
+var color = d3.scale.ordinal()
+    .domain(type(data_V1))
+    .range(["#8A76A6", "#54B5BF", "#8EA65B", "#F27B35", "#F2C14E"]);
+
+/*var color = d3.scale.category20();
+color.domain(type(data))*/
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(150);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) {
+        return +d.Percentage;
     });
-});
 
-// Nueva funcionalidad
-document.querySelectorAll('.category a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); // Evita que el enlace navegue a su href
+change = function(d, i) {
+    var angle = 90 - ((d.startAngle * (180 / Math.PI)) + ((d.endAngle - d.startAngle) * (180 / Math.PI) / 2))
+    svg.transition()
+    .duration(1000)
+    .attr("transform", "translate(" + radius + "," + height / 2 + ") rotate(" + angle + ")")
+    d3.selectAll("path")
+    .transition()
+    .attr("d", arc)
+    d3.select(i)
+    .transition()
+    .duration(1000)
+    .attr("d", arcOver)
+};
 
-        // Obtén la categoría del enlace en el que se hizo clic
-        var category = link.getAttribute('data-category');
+var svg = d3.select("#pieChart").append("svg")
+    .attr("width", '100%')
+    .attr("height", '100%')
+    .attr('viewBox', '0 0 ' + Math.min(width, height) + ' ' + Math.min(width, height))
+    .attr('preserveAspectRatio', 'xMinYMin')
+    .append("g")
+    .attr("transform", "translate(" + radius + "," + height / 2 + ")");
 
-        // Agrega la clase 'unselected' a todos los segmentos
-        document.querySelectorAll('.donut').forEach(function(donut) {
-            donut.classList.add('unselected');
-        });
-
-        // Encuentra el segmento que corresponde a la categoría y elimina las clases 'selected' y 'unselected'
-        var donut = document.querySelector('.donut[data-category="' + category + '"]');
-        donut.classList.remove('selected', 'unselected');
+var g = svg.selectAll("path")
+    .data(pie(data_V1))
+    .enter().append("path")
+    .style("fill", function(d) {
+    return color(d.data.Program);
+    })
+    .attr("d", arc)
+    .style("fill", function(d) {
+    return color(d.data.Program);
+    })
+    .on("click", function(d) {
+    change(d, this);
+    $('.text-container').hide();
+    $('#segmentTitle').replaceWith('<h1 id="segmentTitle">' + d.data.Program + ": " + d.data.Percentage + '%</h1>');
+    $('#')
+    $('#segmentText').replaceWith('<p id="segmentText">' + d.data.Description + '</p>');
+    $('.text-container').fadeIn(400);
     });
-});
 
+document.querySelector('style').textContent += '@media(max-width:767px) {#pieChart { transform: rotate(90deg); transform-origin: 50% 50%; transition: 1s; max-width: 50%; } .text-container { width: 100%; min-height: 0; }} @media(min-width:768px) {#pieChart { transition: 1s;}}'
