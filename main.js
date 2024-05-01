bc6b6152da24f6b2412fc2882ad1f5e13b580a26
JosueGa04 <a01637983@tec.mx>
2024-05-01 07:28:06 -0600

    Commit1may

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var data_V1 = [{
     "Program": "Text",
     "Percentage": 65,
-    "Description": "JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.",
-    "Elements": ["ChatGPT", "Notion.AI", "Llama"]
+    "Description": "These AI models, such as GPT (Generative Pre-trained Transformer), utilize vast amounts of data to understand and replicate human language patterns, enabling them to produce coherent and contextually relevant text across various domains. From generating creative stories and poetry to assisting in customer service interactions and writing code snippets, AI text generation has become ubiquitous.",
+    "Elements": ["ChatGPT", "Notion.AI", "Llama", "GPT-3", "GPT-4", "GPT-5"]
 }, {
     "Program": "Image",
     "Percentage": 55,
@@ -25,15 +25,45 @@ var data_V1 = [{
 var elementData = {
     "ChatGPT": {
         "Description": "ChatGPT es un modelo de lenguaje AI desarrollado por OpenAI.",
-        // Más propiedades aquí...
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
     },
     "Notion.AI": {
         "Description": "Notion.AI es una herramienta de productividad impulsada por AI.",
-        // Más propiedades aquí...
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
     },
     "Photoshop": {
         "Description": "Photoshop es un software de edición de imágenes desarrollado por Adobe.",
-        // Más propiedades aquí...
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
+    },
+    "GIMP": {
+        "Description": "GIMP es un editor de imágenes de código abierto.",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/0/0d/GIMP_icon.svg",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
+    },
+    "Illustrator": {
+        "Description": "Illustrator es un software de diseño gráfico desarrollado por Adobe.",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/2/2d/Illustrator_CC_icon.png",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
+    },
+    "Python": {
+        "Description": "Python es un lenguaje de programación de alto nivel.",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
+    },
+    "Llama": {
+        "Description": "Llama es un modelo de lenguaje AI desarrollado por OpenAI.",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg",
+        "Website": "https://chat.openai.com/",
+        "Video": "https://www.youtube.com/watch?v=o5MutYFWsM8",
     },
 };
 
@@ -56,8 +86,7 @@ var color = d3.scale.ordinal()
     .domain(type(data_V1))
     .range(["#8A76A6", "#54B5BF", "#8EA65B", "#F27B35", "#F2C14E"]);
 
-/*var color = d3.scale.category20();
-color.domain(type(data))*/
+
 
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
@@ -83,6 +112,10 @@ change = function(d, i) {
     .attr("d", arcOver)
 };
 
+var labelArc = d3.svg.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(150);
+
 var svg = d3.select("#pieChart").append("svg")
     .attr("width", '100%')
     .attr("height", '100%')
@@ -103,28 +136,39 @@ var g = svg.selectAll("path")
         return color(d.data.Program);
     })
     .on("click", function(d) {
-        change(d, this);
-        $('.text-container').hide();
-        $('#segmentTitle').replaceWith('<h1 id="segmentTitle">' + d.data.Program + ": " + d.data.Percentage + '%</h1>');
-        $('#segmentText').replaceWith('<p id="segmentText">' + d.data.Description + '</p>');
+    change(d, this);
+    $('.text-container').hide();
+    $('#segmentTitle').replaceWith('<h1 id="segmentTitle">' + d.data.Program + '</h1>');
+    $('#segmentText').replaceWith('<p id="segmentText">' + d.data.Description + '</p>');
     
-        // Crea un elemento de texto para cada elemento en la lista
-        var elementsHtml = d.data.Elements.map(function(element) {
-            return '<span class="element" style="cursor: pointer;">' + element + '</span>';
-        }).join(', ');
-        $('#segmentElements').replaceWith('<p id="segmentElements">Top AIs: ' + elementsHtml + '</p>');
+    // Crea un elemento de texto para cada elemento en la lista
+    var elementsHtml = d.data.Elements.map(function(element) {
+        return '<span class="element element-button" style="cursor: pointer;">' + element + '</span>';
+    }).join(' ');
+    $('#segmentElements').replaceWith('<p id="segmentElements">' + elementsHtml + '</p>');
     
-        $('.text-container').fadeIn(400);
+    $('.text-container').fadeIn(400);
     
-        // Agrega un controlador de eventos de clic a cada elemento
-        $('.element').on('click', function() {
-            // Muestra información específica sobre el elemento cuando se hace clic en él
-            var element = $(this).text();
-            var elementInfo = elementData[element];
-            $('#elementTitle').replaceWith('<h2 id="elementTitle">' + element + '</h2>');
-            $('#elementDescription').replaceWith('<p id="elementDescription">' + elementInfo.Description + '</p>');
-        });
-    });
+    // Agrega la clase 'hidden' a elementTitle, elementDescription, elementImage, elementWebsite y elementVideo
+    $('#elementTitle').addClass('hidden');
+    $('#elementDescription').addClass('hidden');
+    $('#elementImage').addClass('hidden');
+    $('#elementWebsite').addClass('hidden');
+    $('#elementVideo').addClass('hidden');
+    $('.card').addClass('hidden');
+
+    // Agrega un controlador de eventos de clic a cada elemento
+    $('.element').on('click', function() {
+    // Muestra información específica sobre el elemento cuando se hace clic en él
+        var element = $(this).text();
+        var elementInfo = elementData[element];
+        $('#elementTitle').replaceWith('<h2 id="elementTitle">' + element + '</h2>');
+        $('#elementDescription').replaceWith('<p id="elementDescription">' + elementInfo.Description + '</p>');
+        $('#elementImage').replaceWith('<img id="elementImage" src="' + elementInfo.Image + '" alt="' + element + '">').removeClass('hidden');
+        $('#elementWebsite').replaceWith('<a id="elementWebsite" href="' + elementInfo.Website + '">Sitio web</a>').removeClass('hidden');
+        $('.card').removeClass('hidden'); // Muestra el div .card
+});
+});
 
 
 document.querySelector('style').textContent += '@media(max-width:767px) {#pieChart { transform: rotate(90deg); transform-origin: 50% 50%; transition: 1s; max-width: 50%; } .text-container { width: 100%; min-height: 0; }} @media(min-width:768px) {#pieChart { transition: 1s;}}'
